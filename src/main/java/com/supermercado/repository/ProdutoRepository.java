3d28ef170107f8ed35aaab6c383846858ea75e3e
mcpereir <marcio.mcpereira77@gmail.com>
2022-09-26 23:03:29 -0300

    Alteração classe de select

--- src/main/java/com/supermercado/repository/ProdutoRepository.java
+++ src/main/java/com/supermercado/repository/ProdutoRepository.java
@@ -3,6 +3,8 @@ package com.supermercado.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.supermercado.model.Produto;
@@ -14,5 +16,24 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	public Produto findByDescricao(String descricao);
 	public List<Produto> findByDescricaoContaining(String descricao);
 	public Produto findByCodigo(Integer cod);
+	
+	@Query("SELECT p FROM Produto p where p.descricao = :descricao")
+    public Produto findByDescription(@Param("description") String descricao);
+	
+	
+/*	
+	@Query(value= "select 	cp., cs. "
+			+ "from		calendario_profissional cp "
 
+			+ "		INNER JOIN calendario_servico cs ON "
+			+ "				cp.servico 		= cs.servico and "
+			+ "				cp.data 		= cs.data and "
+			+ "				cp.hora_inicial = cs.hora_inicial and "
+			+ "				cp.hora_final 	= cs.hora_final "
+
+			+ "where	cp.servico = ?1 and "
+			+ "			cp.data = ?2 "
+			, nativeQuery = true ) 
+     List<CalendarioProfissional> findServicoData(Servico servico, LocalDate data);
+*/
 }
